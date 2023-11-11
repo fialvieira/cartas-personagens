f8940895e28f2864f604d9b812b06ce0f07d998f
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2023-11-11 11:17:56 -0300

    Fim primeira lição.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}
+import {Personagem} from "./modules/personagem.js";
 
-const personagemJose = {
+/*const personagemJose = {
     nome: 'Jose',
     vida: 7,
     mana: 6,
     level: 3,
     tipo: 'Arqueiro',
+}*/
+
+const personagemAna = {
+    nome: 'Ana',
+    vida: 8,
+    mana: 10,
+    level: 5,
+    tipo: 'Bárbaro'
 }
 
+const personagemPedrinho = new Personagem()
+personagemPedrinho.nome = 'Pedrinho'
+personagemPedrinho.mana = 12
+personagemPedrinho.vida = 7
+personagemPedrinho.level = 5
+personagemPedrinho.tipo = 'Mago'
+
+const personagemJose = new Personagem()
+personagemJose.nome = 'José'
+personagemJose.tipo = 'Arqueiro'
+personagemJose.level = 3
+
+console.log('Insignia de ' + personagemPedrinho.nome + ': ' + personagemPedrinho.obterInsignia())
+console.log('Insignia de ' + personagemJose.nome + ': ' + personagemJose.obterInsignia())
---
+++ src/modules/personagem.js
@@ -0,0 +1,15 @@
+export class Personagem {
+    nome
+    vida
+    mana
+    level
+    tipo
+    descricao
+
+    obterInsignia() {
+        if (this.level >= 5) {
+            return `Implacável ${this.tipo}`
+        }
+        return `${this.tipo} iniciante`
+    }
+}
